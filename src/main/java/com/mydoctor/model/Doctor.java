04ea0d1f366f61432a8a85d2d24c7ccaed873bf8
DESKTOP-9AIRUPC\Snowman <pinkaewklaysuwan>
2016-11-14 19:36:28 +0700

    doctor class added

---
+++ src/main/java/com/mydoctor/model/Doctor.java
@@ -0,0 +1,65 @@
+package com.mydoctor.model;
+
+import java.util.ArrayList;
+
+public class Doctor extends User {
+	
+	private String department;
+	private String name,surname;
+	private ArrayList<Schedule> schedule_arr;
+	public Doctor() {
+		// TODO Auto-generated constructor stub
+		super();
+	}
+	public Doctor(String username,String password, String type) {
+		// TODO Auto-generated constructor stub
+		super(username, password, type);
+	}
+	public Doctor(String username,String password, String type,String name ,String surname, String department) {
+		// TODO Auto-generated constructor stub
+		super(username, password, type);
+		this.department = department;
+		this.name = name;
+		this.surname = surname;
+	}
+	public String getName() {
+		return name;
+	}
+	public String getSurname() {
+		return surname;
+	}
+	public String getDepartment() {
+		return department;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public void setSurname(String surname) {
+		this.surname = surname;
+	}
+	public void setDepartment(String department) {
+		this.department = department;
+	}
+	
+	public void addSchedule(Schedule schedule){
+		schedule_arr.add(schedule);
+		
+	}
+	public void remove(String schedule_id){
+		for(int i=0; i < schedule_arr.size() ;i++){
+			if( schedule_id.equals(schedule_arr.get(i).getSchedule_id() ) ){
+				schedule_arr.remove(i);
+			}
+		}
+	}
+	public ArrayList<Schedule> getSchedule_arr() {
+		return schedule_arr;
+	}
+	public void setSchedule_arr(ArrayList<Schedule> schedule_arr) {
+		this.schedule_arr = schedule_arr;
+	}
+	
+	
+	
+
+}
