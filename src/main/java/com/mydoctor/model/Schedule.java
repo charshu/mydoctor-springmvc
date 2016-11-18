fc6a01b14fd839ed8f985161c37c463cab86fe5c
Vachirakorn Rungsikawanich <vachirakorn@hotmail.com>
2016-11-18 15:10:29 +0700

    แก้เวลาเป็น timestamp ให้แสดงผลถูก

--- src/main/java/com/mydoctor/model/Schedule.java
+++ src/main/java/com/mydoctor/model/Schedule.java
@@ -1,6 +1,7 @@
 package com.mydoctor.model;
 
 import java.sql.Date;
+import java.sql.Timestamp;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 
@@ -8,36 +9,35 @@ public class Schedule {
 	
 	private String id;
 	//Date stores in format milliseconds -> 1000022311231
-	private Date start,end;
-	private final DateFormat df = new SimpleDateFormat("dd-MM-yyyy hh:mm:ss");
+	private Timestamp start,end;
+	private final DateFormat df = new SimpleDateFormat("dd-MM-yyyy HH:mm:ss");
 	
 	public Schedule(){
 	
 	}
 
-	public Schedule(String id, Date start, Date end) {
+	public Schedule(String id, Timestamp start, Timestamp end) {
 		super();
 		this.id = id;
 		this.start = start;
 		this.end = end;
 	}
-
 	public String getId() {
 		return id;
 	}
 	public void setId(String id) {
 		this.id = id;
 	}
-	public Date getStart() {
+	public Timestamp getStart() {
 		return start;
 	}
-	public void setStart(Date start) {
+	public void setStart(Timestamp start) {
 		this.start = start;
 	}
-	public Date getEnd() {
+	public Timestamp getEnd() {
 		return end;
 	}
-	public void setEnd(Date end) {
+	public void setEnd(Timestamp end) {
 		this.end = end;
 	}
 
@@ -45,7 +45,6 @@ public class Schedule {
 	public String toString() {
 		return "The schedule id : " + this.id + " Start time : " + df.format(start) + " End time : " + df.format(end);
 	}
-	
 	
 	public boolean isWithinRange(Date date){
 		return date.after(start) && date.before(end);
